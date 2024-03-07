24e4db35770b4ad29f7d654d7ade40054efdf323
mmwandera <mmwandera74@gmail.com>
2024-03-07 12:54:19 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -52,6 +52,51 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+#############################################################
+# Generate these resources:
+
+# Login is located at /login
+class Login(Resource):
+
+    # It has one route, post()
+    def post(self):
+        # post() gets a username from request's JSON.
+        username = request.get_json()['username']
+        # post() retrieves the user by username.
+        user = User.query.filter(User.username == username).first()
+        # post() sets the session's user_id value to the user's id.
+        session['user_id'] = user.id
+        # post() returns the user as JSON with a 200 status code.
+        return user.to_dict(), 200
+
+# Logout is located at /logout.
+class Logout(Resource):
+
+    # It has one route, delete().
+    def delete(self):
+        # delete() removes the user_id value from the session.
+        session['user_id'] = None
+        # delete() returns no data and a 204 (No Content) status code.
+        return {}, 204
+
+# CheckSession is located at /check_session.
+class CheckSession(Resource):
+
+    # It has one route, get().
+    def get(self):
+        # get() retrieves the user_id value from the session.
+        user_id = session.get('user_id')
+        # If the session has a user_id, get() returns the user as JSON with a 200 status code.
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        # If the session does not have a user_id, get() returns no data and a 401 (Unauthorized) status code.
+        return {}, 401
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
